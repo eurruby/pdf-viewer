6e0cd07f757aa6693aa1cf8e64d0ab0ff07079ec
eurruby <1322370+eurruby@users.noreply.github.com>
2025-03-03 16:42:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(443, function(){
-    console.log('Server running on 8080...');
+    console.log('Server running...');
 });
