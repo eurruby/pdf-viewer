faedc9f889c1b78b26c3d469c64fcf4c9e16f472
eurruby <1322370+eurruby@users.noreply.github.com>
2025-03-03 15:40:33 +0100

    Update viewer.js

--- external/pdfjs-2.1.266-dist/web/viewer.js
+++ external/pdfjs-2.1.266-dist/web/viewer.js
@@ -5132,7 +5132,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER
   },
   defaultUrl: {
-    value: 'compressed.tracemonkey-pldi-09.pdf',
+    value: 'outfile.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
